a65dce04b5d20d0b136ad1018e5f5fa50bd58adf
wzhzlh“git config --global user.email 2902981758@qq.com“ <2902981758@qq.com>
2026-02-05 00:08:19 +0800

    新增灵足，舵机驱动

---
+++ MyLib/kfc_grasp and release.h
@@ -0,0 +1,8 @@
+#ifndef __KFC_GRASP_AND_RELEASE_H__
+#define __KFC_GRASP_AND_RELEASE_H__
+
+#include "tim.h"
+
+int32_t RAMP_self( int32_t final, int32_t now, int32_t ramp );
+void Servo_control(void) ;  
+#endif
