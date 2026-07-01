519e891716af731cba143567dd5e75374af6c79d
Awyly <15630332621@163.com>
2026-07-01 11:06:45 +0800

    数据包包头更改

--- MyTask/arm.c
+++ MyTask/arm.c
@@ -19,7 +19,7 @@ extern UART_HandleTypeDef huart3;
 extern int Temp_Servo_Target[3];//舵机的期望占空比，应该用来接收上位机的期望
 extern int32_t Servo_assignment[3];//舵机的预设置
 float expect_ = 0;
-state_pack_t state_pack = {.pack_type = 0x01};
+state_pack_t state_pack = {.pack_type = 0x10};
 target_pack_t target_pack = {.pack_type = 0x01};//用来接收上位机发来的数据包
 
 uint8_t receiving_number = 0;
@@ -343,7 +343,7 @@ void HAL_UARTEx_RxEventCallback(
                 }
             }
         }
-        HAL_UART_DMAStop(&huart3);
+        // HAL_UART_DMAStop(&huart3);
         HAL_UARTEx_ReceiveToIdle_DMA( &huart3,vl53_rx_buf,VL53_RX_SIZE);
 		__HAL_DMA_DISABLE_IT(huart3.hdmarx,DMA_IT_HT);
     }
@@ -354,7 +354,7 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
     if (huart->Instance == USART3)
     {
         error ++;
-        HAL_UART_DMAStop(&huart3);
+        // HAL_UART_DMAStop(&huart3);
         HAL_UARTEx_ReceiveToIdle_DMA( &huart3,vl53_rx_buf,VL53_RX_SIZE);
 		__HAL_DMA_DISABLE_IT(huart3.hdmarx,DMA_IT_HT);
     }
